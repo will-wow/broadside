b54e7d30314e4bd325ffbbe9b86df6f8426c668c
Will Ockelmann-Wagner <will.ockelmann.wagner@gmail.com>
2018-10-04 00:10:39 -0700

    center objects

--- broadside-front/src/game/position.ts
+++ broadside-front/src/game/position.ts
@@ -13,5 +13,5 @@ export const vw = postfix("vw");
 export const vh = postfix("vh");
 
 export const translate = (maxX: number, maxY: number, { x, y }: t): string =>
-  `translate(${vw((x / maxX) * 90)}, ${vh((y / maxY) * 90)})`;
+  `translate(-50%, -50%) translate(${vw((x / maxX) * 90)}, ${vh((y / maxY) * 90)})`;
 export const rotate = ({ heading }: t): string => `rotate(${heading + 90}deg)`;
